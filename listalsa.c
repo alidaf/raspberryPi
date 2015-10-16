a6a9bc704cc936b179cebd1d18d0b6c7e8959d63
Linux User <darren@alidaf.co.uk>
2015-10-16 20:30:49 +0100

    Changed data structures

--- listalsa.c
+++ listalsa.c
@@ -8,31 +8,47 @@
 #include <string.h>
 #include <alsa/asoundlib.h>
 
+struct alsaStruct
+{
+	int deviceNum;
+	int subDevice;
+	char pcmDevice[64];
+};
+
 int main()
 {
 
+	struct alsaStruct alsaInfo =
+	{
+		.deviceNum = -1,
+		.subDevice = -1,
+	};
+
 	int err;
-	int cardNum = -1;
 	snd_ctl_t *cardHandle;
 	snd_ctl_card_info_t *cardInfo;
-	char pcmDevice[64];
 
 	while (1)
 	{
-
-		if (( err = snd_card_next( &cardNum )) < 0 ) // ALSA returns -1 on error.
+		// Find next card number. If < 0 then returns 1st card.
+		if (( err = snd_card_next( &alsaInfo.deviceNum )) < 0 ) // ALSA returns -1 on error.
 		{
 			printf( "Error: %s\n", snd_strerror( err )); // Prints ALSA error message.
 			break;
 		}
 
-		if ( cardNum < 0 ) break; // ALSA returns -1 if no card found.
+		if ( alsaInfo.deviceNum < 0 ) break; // ALSA returns -1 if no card found.
 
 		{
-			sprintf( pcmDevice, "hw:%i", cardNum ); // Concatenate strings to get PCM device.
-			if (( err = snd_ctl_open( &cardHandle, pcmDevice, 0 )) < 0 )
+			// Concatenate strings to get PCM device.
+			sprintf( alsaInfo.pcmDevice, "hw:%i", alsaInfo.deviceNum );
+			printf( "Device name = %s\n", alsaInfo.pcmDevice );
+			if (( err = snd_ctl_open( &cardHandle, alsaInfo.pcmDevice, 0 )) < 0 )
 			{
-				printf( "Can't open card %i (%s): %s\n", cardNum, pcmDevice, snd_strerror( err ));
+				printf( "Can't open card %i (%s): %s\n",
+						alsaInfo.deviceNum,
+						alsaInfo.pcmDevice,
+						snd_strerror( err ));
 				continue;
 			}
 		}
@@ -41,9 +57,15 @@ int main()
 		snd_ctl_card_info_alloca( &cardInfo );
 
 		if (( err = snd_ctl_card_info( cardHandle, cardInfo )) < 0 )
-			printf( "Error getting info for card %i (%s): %s\n", cardNum, pcmDevice, snd_strerror( err ));
+			printf( "Error getting info for card %i (%s): %s\n",
+					alsaInfo.deviceNum,
+					alsaInfo.pcmDevice,
+					snd_strerror( err ));
 		else
-			printf( "Card %i (%s) = %s\n", cardNum, pcmDevice, snd_ctl_card_info_get_name( cardInfo ));
+			printf( "Card %i (%s) = %s\n", 
+					alsaInfo.deviceNum,
+					alsaInfo.pcmDevice,
+					snd_ctl_card_info_get_name( cardInfo ));
 
 		snd_ctl_close( cardHandle ); // Relinquish control.
 	}
