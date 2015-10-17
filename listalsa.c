af1e33afe5365fe3509f80072d0bb21107c63cee
Linux User <darren@alidaf.co.uk>
2015-10-17 11:10:35 +0100

    Main device working

--- listalsa.c
+++ listalsa.c
@@ -12,8 +12,60 @@ struct alsaStruct
 {
 	int deviceNum;
 	int subDevice;
-	char pcmDevice[64];
+	const char *deviceName;
+	const char *subName;
+	char deviceRef[64];
 };
+
+/************************************************************************/
+/*																		*/
+/* Print info routine.													*/
+/*																		*/
+/************************************************************************/
+
+void printInfo( struct alsaStruct alsaInfo, int body )
+{
+
+	if ( body == 0 )
+	{
+		printf( "\t+--------------------------+" );
+		printf( "--------------------------+\n" );
+		printf( "\t| Device                   |" );
+		printf( " Sub Device               |\n" );
+		printf( "\t+-----+--------------------+" );
+		printf( "-----+--------------------+\n" );
+		printf( "\t| No. | Name               |" );
+		printf( " No. | Name               |\n" );
+		printf( "\t+-----+--------------------+" );
+		printf( "-----+--------------------+\n" );
+	}
+	else
+		printf( "\t| %3i | %-12s       | %3i | %-12s       |\n",
+						alsaInfo.deviceNum,
+						alsaInfo.deviceName,
+						alsaInfo.subDevice,
+						alsaInfo.subName );
+
+};
+
+/************************************************************************/
+/*																		*/
+/* Print error message.													*/
+/*																		*/
+/************************************************************************/
+
+void printError( int error )
+{
+
+	printf( "Error: %s\n", snd_strerror( error ));
+
+};
+
+/************************************************************************/
+/*																		*/
+/* Main routine.														*/
+/*																		*/
+/************************************************************************/
 
 int main()
 {
@@ -22,33 +74,40 @@ int main()
 	{
 		.deviceNum = -1,
 		.subDevice = -1,
+		.deviceName = "",
+		.subName = "",
 	};
 
-	int err;
+	int errNum;
 	snd_ctl_t *cardHandle;
 	snd_ctl_card_info_t *cardInfo;
+
+	// Print header.
+	printInfo( alsaInfo, 0 );
 
 	while (1)
 	{
 		// Find next card number. If < 0 then returns 1st card.
-		if (( err = snd_card_next( &alsaInfo.deviceNum )) < 0 ) // ALSA returns -1 on error.
+		errNum = snd_card_next( &alsaInfo.deviceNum );
+		if ( errNum < 0 )
 		{
-			printf( "Error: %s\n", snd_strerror( err )); // Prints ALSA error message.
+			printError( errNum );
 			break;
 		}
 
-		if ( alsaInfo.deviceNum < 0 ) break; // ALSA returns -1 if no card found.
+		// ALSA returns -1 if no card found.
+		if ( alsaInfo.deviceNum < 0 ) break;
 
 		{
 			// Concatenate strings to get PCM device.
-			sprintf( alsaInfo.pcmDevice, "hw:%i", alsaInfo.deviceNum );
-			printf( "Device name = %s\n", alsaInfo.pcmDevice );
-			if (( err = snd_ctl_open( &cardHandle, alsaInfo.pcmDevice, 0 )) < 0 )
+			sprintf( alsaInfo.deviceRef, "hw:%i", alsaInfo.deviceNum );
+
+			//  Open device.
+			errNum = snd_ctl_open( &cardHandle, alsaInfo.deviceRef, 0 );
+
+			if ( errNum < 0 )
 			{
-				printf( "Can't open card %i (%s): %s\n",
-						alsaInfo.deviceNum,
-						alsaInfo.pcmDevice,
-						snd_strerror( err ));
+				printError( errNum );
 				continue;
 			}
 		}
@@ -56,19 +115,19 @@ int main()
 		// Initialise card info type.
 		snd_ctl_card_info_alloca( &cardInfo );
 
-		if (( err = snd_ctl_card_info( cardHandle, cardInfo )) < 0 )
-			printf( "Error getting info for card %i (%s): %s\n",
-					alsaInfo.deviceNum,
-					alsaInfo.pcmDevice,
-					snd_strerror( err ));
+		errNum = snd_ctl_card_info( cardHandle, cardInfo );
+		if ( errNum < 0 )
+			printError( errNum );
 		else
-			printf( "Card %i (%s) = %s\n", 
-					alsaInfo.deviceNum,
-					alsaInfo.pcmDevice,
-					snd_ctl_card_info_get_name( cardInfo ));
+		{
+			alsaInfo.deviceName = snd_ctl_card_info_get_name( cardInfo );
+			printInfo( alsaInfo, 1 );
+		}
 
 		snd_ctl_close( cardHandle ); // Relinquish control.
 	}
 
 	snd_config_update_free_global(); // Close cleanly.
+
+	return 0;
 }
