e172d6677459a4264173a801bc7b107e2d8bfc65
Linux User <darren@alidaf.co.uk>
2015-10-17 17:49:37 +0100

    Modified based on aplay code

--- listalsa.c
+++ listalsa.c
@@ -10,11 +10,13 @@
 
 struct alsaStruct
 {
+	int cardNum;
 	int deviceNum;
-	int subDevice;
+	int subDeviceNum;
+	const char *cardName;
 	const char *deviceName;
-	const char *subName;
-	char deviceRef[64];
+	const char *subDeviceName;
+	char deviceID[64];
 };
 
 /************************************************************************/
@@ -29,23 +31,29 @@ void printInfo( struct alsaStruct alsaInfo, int body )
 	if ( body == 0 )
 	{
 		printf( "\t+--------------------------+" );
+		printf( "--------------------------+" );
 		printf( "--------------------------+\n" );
-		printf( "\t| Device                   |" );
+		printf( "\t| Card                     |" );
+		printf( " Device                   |" );
 		printf( " Sub Device               |\n" );
 		printf( "\t+-----+--------------------+" );
+		printf( "-----+--------------------+" );
 		printf( "-----+--------------------+\n" );
 		printf( "\t| No. | Name               |" );
+		printf( " No. | Name               |" );
 		printf( " No. | Name               |\n" );
 		printf( "\t+-----+--------------------+" );
+		printf( "-----+--------------------+" );
 		printf( "-----+--------------------+\n" );
 	}
 	else
-		printf( "\t| %3i | %-12s       | %3i | %-12s       |\n",
+		printf( "\t| %3i | %-12s       | %3i | %-12s       | %3i | %-12s       |\n",
+						alsaInfo.cardNum,
+						alsaInfo.cardName,
 						alsaInfo.deviceNum,
 						alsaInfo.deviceName,
-						alsaInfo.subDevice,
-						alsaInfo.subName );
-
+						alsaInfo.subDeviceNum,
+						alsaInfo.subDeviceName );
 };
 
 /************************************************************************/
@@ -67,67 +75,101 @@ void printError( int error )
 /*																		*/
 /************************************************************************/
 
-int main()
+void main()
 {
 
 	struct alsaStruct alsaInfo =
 	{
+		.cardNum = -1,
 		.deviceNum = -1,
-		.subDevice = -1,
+		.subDeviceNum = -1,
+		.cardName = "",
 		.deviceName = "",
-		.subName = "",
+		.subDeviceName = "",
+		.deviceID = "",
 	};
 
 	int errNum;
+	int deviceCount;
+	int loop;
+
 	snd_ctl_t *cardHandle;
 	snd_ctl_card_info_t *cardInfo;
+	snd_pcm_info_t *deviceInfo;
+
+	// Initialise ALSA card and device types.
+	snd_ctl_card_info_alloca( &cardInfo );
+	snd_pcm_info_alloca( &deviceInfo );
 
 	// Print header.
 	printInfo( alsaInfo, 0 );
 
-	while (1)
+	// Find next card number. If < 0 then returns 1st card.
+	errNum = snd_card_next( &alsaInfo.cardNum );
+	if ( errNum < 0 ) return;
+	// ALSA returns -1 if no card found.
+	if ( alsaInfo.cardNum < 0 ) return;
+
+	while ( alsaInfo.cardNum >= 0 )
 	{
-		// Find next card number. If < 0 then returns 1st card.
-		errNum = snd_card_next( &alsaInfo.deviceNum );
-		if ( errNum < 0 )
-		{
-			printError( errNum );
-			break;
-		}
 
-		// ALSA returns -1 if no card found.
-		if ( alsaInfo.deviceNum < 0 ) break;
+		// Concatenate strings to get card's control interface.
+		sprintf( alsaInfo.deviceID, "hw:%i", alsaInfo.cardNum );
 
-		{
-			// Concatenate strings to get PCM device.
-			sprintf( alsaInfo.deviceRef, "hw:%i", alsaInfo.deviceNum );
+		//  Try to open card.
+		errNum = snd_ctl_open( &cardHandle, alsaInfo.deviceID, 0 );
+		if ( errNum < 0 ) continue;
 
-			//  Open device.
-			errNum = snd_ctl_open( &cardHandle, alsaInfo.deviceRef, 0 );
-
-			if ( errNum < 0 )
-			{
-				printError( errNum );
-				continue;
-			}
-		}
-
-		// Initialise card info type.
-		snd_ctl_card_info_alloca( &cardInfo );
-
+		// Fill snd_ctl_card_info_t.
 		errNum = snd_ctl_card_info( cardHandle, cardInfo );
 		if ( errNum < 0 )
-			printError( errNum );
-		else
 		{
-			alsaInfo.deviceName = snd_ctl_card_info_get_name( cardInfo );
-			printInfo( alsaInfo, 1 );
+			snd_ctl_close( cardHandle );
+//			continue;
+		}
+		else alsaInfo.cardName = snd_ctl_card_info_get_name( cardInfo );
+
+		while (1)
+		{
+
+			// Find next device number. If < 0 then returns 1st device.
+			errNum = snd_ctl_pcm_next_device( cardHandle, &alsaInfo.deviceNum );
+
+			if ( alsaInfo.deviceNum < 0 ) break;
+
+			snd_pcm_info_set_device( deviceInfo, alsaInfo.deviceNum );
+			snd_pcm_info_set_subdevice( deviceInfo, 0 );
+			snd_pcm_info_set_stream( deviceInfo, SND_PCM_STREAM_PLAYBACK );
+
+			errNum = snd_ctl_pcm_info( cardHandle, deviceInfo );
+			if ( errNum < 0 ) continue;
+
+//		}
+
+			alsaInfo.cardName = snd_ctl_card_info_get_name( cardInfo );
+			alsaInfo.deviceName = snd_pcm_info_get_name( deviceInfo );
+
+			// Get number of sub devices.
+			deviceCount = snd_pcm_info_get_subdevices_count( deviceInfo );
+
+			for ( loop = 0; loop < (int) deviceCount; loop++ )
+			{
+				snd_pcm_info_set_subdevice( deviceInfo, loop );
+				errNum = snd_ctl_pcm_info( cardHandle, deviceInfo );
+				alsaInfo.subDeviceNum = loop;
+				alsaInfo.subDeviceName = snd_pcm_info_get_subdevice_name( deviceInfo );
+
+				// Print card and device information.
+				printInfo( alsaInfo, 1 );
+			}
+
 		}
 
-		snd_ctl_close( cardHandle ); // Relinquish control.
 	}
+
+	snd_ctl_close( cardHandle );
 
 	snd_config_update_free_global(); // Close cleanly.
 
-	return 0;
+	return;
 }
