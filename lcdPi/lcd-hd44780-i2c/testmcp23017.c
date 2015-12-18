aefa5413e27310fd02a16be69963ae6e472d13ff
Linux User <darren@alidaf.co.uk>
2015-12-18 13:23:18 +0000

    Added simple read test

--- lcdPi/lcd-hd44780-i2c/testmcp23017.c
+++ lcdPi/lcd-hd44780-i2c/testmcp23017.c
@@ -54,15 +54,15 @@
 
                         +-----------( )-----------+
                         |  Fn  | pin | pin |  Fn  |
-             100R  LED  |------+-----+-----+------|  LED  100R
-      GND<--/\/\/--|<|--| GPB0 |  01 | 28  | GPA7 |--|>|--/\/\/-->GND
-      GND<--/\/\/--|<|--| GPB1 |  02 | 27  | GPA6 |--|>|--/\/\/-->GND
-      GND<--/\/\/--|<|--| GPB2 |  03 | 26  | GPA5 |--|>|--/\/\/-->GND
-      GND<--/\/\/--|<|--| GPB3 |  04 | 25  | GPA4 |--|>|--/\/\/-->GND
-      GND<--/\/\/--|<|--| GPB4 |  05 | 24  | GPA3 |--|>|--/\/\/-->GND
-      GND<--/\/\/--|<|--| GPB5 |  06 | 23  | GPA2 |--|>|--/\/\/-->GND
-      GND<--/\/\/--|<|--| GPB6 |  07 | 22  | GPA1 |--|>|--/\/\/-->GND
-      GND<--/\/\/--|<|--| GPB7 |  08 | 21  | GPA0 |--|>|--/\/\/-->GND
+             100R  LED  |------+-----+-----+------| 8 port dip switch
+      GND<--/\/\/--|<|--| GPB0 |  01 | 28  | GPA7 |---/ --> +3.3V
+      GND<--/\/\/--|<|--| GPB1 |  02 | 27  | GPA6 |---/ --> +3.3V
+      GND<--/\/\/--|<|--| GPB2 |  03 | 26  | GPA5 |---/ --> +3.3V
+      GND<--/\/\/--|<|--| GPB3 |  04 | 25  | GPA4 |---/ --> +3.3V
+      GND<--/\/\/--|<|--| GPB4 |  05 | 24  | GPA3 |---/ --> +3.3V
+      GND<--/\/\/--|<|--| GPB5 |  06 | 23  | GPA2 |---/ --> +3.3V
+      GND<--/\/\/--|<|--| GPB6 |  07 | 22  | GPA1 |---/ --> +3.3V
+      GND<--/\/\/--|<|--| GPB7 |  08 | 21  | GPA0 |---/ --> +3.3V
               +3.3V <---|  VDD |  09 | 20  | INTA |
                 GND <---|  VSS |  10 | 19  | INTB |
                         |   NC |  11 | 18  | RST  |---> +3.3V.
@@ -95,17 +95,17 @@ int main()
     int8_t err = 0; // Return code.
     int8_t num = 1; // Number of MCP23017 devices.
 
-    // Initialise chip. Call for each device.
-    err = mcp23017Init( 0x20, BANK_0 );
+    // Initialise MCP23017. Only using 1 but should work for up to 8.
+    err = mcp23017Init( 0x20 );
     if ( err < 0 )
         {
             printf( "Couldn't init.\n" );
             return -1;
         };
 
-    uint8_t i;
     // Print properties for each device.
     printf( "Properties.\n" );
+    uint8_t i;
     for ( i = 0; i < num; i++ )
     {
         printf( "\tDevice %d:\n", i );
@@ -113,11 +113,53 @@ int main()
         printf( "\tAddress = 0x%02x,\n", mcp23017[i]->addr );
         printf( "\tBank mode = %1d.\n", mcp23017[i]->bank );
     };
+    printf( "\n" );
 
-    // Write a byte to light LEDs corresponding to byte value.
-    for ( i = 0; i <= 0xff; i++ )
+    // Set direction of GPIOs and clear latches.
+    mcp23017WriteRegisterByte( mcp23017[0], IODIRA, 0xff );
+    mcp23017WriteRegisterByte( mcp23017[0], IODIRB, 0x00 );
+    // Writes to latches are the same as writes to GPIOs.
+    mcp23017WriteRegisterByte( mcp23017[0], OLATA, 0x00 );
+    mcp23017WriteRegisterByte( mcp23017[0], OLATB, 0x00 );
+
+    // Start off with BANK = 0, switch and switch again.
+    uint8_t j;
+    for ( j = 0; j < 3; j++ )
     {
-        mcp23017WriteRegisterWord( mcp23017[0]->id, OLATB, i );
+        // MCP23017 initialises with BANK=0.
+        printf( "BANK = %d.\n", mcp23017[0]->bank );
+        if ( mcp23017[0]->bank == 0 )
+            mcp23017WriteRegisterByte( mcp23017[0], IOCONA, 0x00 );
+        else
+            mcp23017WriteRegisterByte( mcp23017[0], IOCONA, 0x80 );
+
+        // Write a byte to light LEDs corresponding to byte value.
+        for ( i = 0; i <= 0xff; i++ )
+        {
+            mcp23017WriteRegisterWord( mcp23017[0], OLATB, i );
+            sleep( 1 );
+        }
+
+        // Reset all LEDs.
+        mcp23017WriteRegisterByte( mcp23017[0], OLATB, 0x00 );
+
+        // Switch banks and loop back around.
+        mcp23017[0]->bank = !mcp23017[0]->bank;
+    }
+
+    uint8_t data, last = 0x00;
+    // Now test input.
+    printf( "Reading inputs on PORT A.\n" );
+    while ( 1 )
+    {
+        // Read switches and write byte value to LEDs.
+        data = mcp23017ReadRegisterByte( mcp23017[0], GPIOA );
+        if ( data != last )
+        {
+            printf( "Input changed to 0x%02x.\n", data );
+            last = data;
+        }
+        mcp23017WriteRegisterByte( mcp23017[0], OLATB, data );
         sleep( 1 );
     }
 }
